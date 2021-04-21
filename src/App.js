588555daa6be674fe7d2fc0afd4b79841b180a4f
chris097 <chrisfidel.international@gmail.com>
2021-04-21 14:32:34 +0100

    add new file and first code with material design

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography 
+        variant="h1"
+        color="secondary"
+        align="center"
+      >
+        Hello world
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
