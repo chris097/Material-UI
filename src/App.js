8c42cd5b99ab2d0428bac60b270b5baea7bacce0
chris097 <chrisfidel.international@gmail.com>
2021-04-24 06:47:47 +0100

    Day two: forms

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 
+const useStyles = makeStyles({
+  btn:{ backgroundColor: 'yellow' }
+})
+
 function App() {
+  const classes = useStyles()
   return (
     <Container className="App">
       <Typography> 
         Hello world
       </Typography>
-      <Button type="submit" variant="contained" color="primary">Click me</Button>
+      <Button className={classes.btn} type="submit" variant="contained" color="primary">Click me</Button>
       <ButtonGroup>
         <Button variant="contained" disabled>One</Button>
         <Button variant="contained" color="secondary">Two</Button>
-        <Button variant="contained" color="primary">Three</Button>
+        <Button>Three</Button>
       </ButtonGroup>
+
+      <TextField variant="outlined" label="Me" />
     </Container>
   );
 }
