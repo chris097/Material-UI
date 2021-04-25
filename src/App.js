a2e28bd563e8c44575b4885190dab70dee638be3
chris097 <chrisfidel.international@gmail.com>
2021-04-25 05:32:26 +0100

    create a form with radio button

--- src/App.js
+++ src/App.js
@@ -1,32 +1,94 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-undef */
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import { makeStyles } from '@material-ui/core';
+// import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { FormControl, FormControlLabel, FormLabel, makeStyles } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
 import './App.css';
 
 const useStyles = makeStyles({
-  btn:{ backgroundColor: 'yellow' }
+  feild:{
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block'
+  }
 })
 
 function App() {
-  const classes = useStyles()
+  const classes = useStyles();
+
+  const [name, setName] = useState('');
+  const [details, setDetails] = useState('');
+  const [nameError, setNameError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+  const [category, setCategory] = useState('age');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNameError(false);
+    setDetailsError(false)
+
+    if(name && details){
+      console.log(name, details, category)
+    }else if(nameError == '' && detailsError == ''){
+      setNameError(true)
+      setDetailsError(true)
+    }
+
+  }
+
   return (
     <Container className="App">
-      <Typography> 
-        Hello world
-      </Typography>
-      <Button className={classes.btn} type="submit" variant="contained" color="primary">Click me</Button>
-      <ButtonGroup>
-        <Button variant="contained" disabled>One</Button>
-        <Button variant="contained" color="secondary">Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
-
-      <TextField variant="outlined" label="Me" />
+    <Typography
+      variant="h5"
+      color="textSecondary"
+      gutterBottom
+    >
+      Create a note
+    </Typography>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField 
+        onChange={e => setName(e.target.value)}
+        className={classes.feild}
+        label="Name" 
+        variant="outlined" 
+        fullWidth
+        required
+        error={nameError}
+        />
+        <TextField 
+        onChange={e => setDetails(e.target.value)}
+        className={classes.feild}
+        label="Details" 
+        variant="outlined" 
+        fullWidth
+        multiline
+        rows={4}
+        required
+        error={detailsError}
+        />
+        <FormControl className={classes.feild}>
+          <FormLabel>Name Category</FormLabel>
+          <RadioGroup value={category} onChange={ e => setCategory(e.target.value)}>
+            <FormControlLabel value="age" control={<Radio />} label="Age" />
+            <FormControlLabel value="occupation" control={<Radio />} label="Occupation" />
+            <FormControlLabel value="location" control={<Radio />} label="Location" />
+            <FormControlLabel value="state" control={<Radio />} label="State" />
+          </RadioGroup>
+        </FormControl>
+        <Button 
+          type="submit"
+          variant="contained" 
+          color="secondary"
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
